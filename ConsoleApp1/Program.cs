d00f1abaa4cddd659563022373a219a8d7be2af0
AllaHrachova <alla_krivosheina@Ukr.net>
2021-04-04 07:14:28 +0300

    commit 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово");
+
+            string word = Console.ReadLine();
+            bool isValid = word.All(Char.IsLetter);
+            if (isValid == false)
+            {
+                Console.WriteLine("Нужно ввести одно слово из букв");
+            }
+            else
+            {
+                if (IsPalindrom(word) == true)
+                {
+                    Console.WriteLine("Палиндром");
+                }
+                else
+                {
+                    Console.WriteLine("Не палиндром");
+                }
+
+                Console.ReadLine();
+            }
+        }
+        public static bool IsPalindrom(string word)
+        {
+            bool r = true;
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                int n = word.Length - 1 - i;
+
+                if (word[i] != word[n])
+                {
+                    r = false;
+                }
+            }
+            return r;
+        }
+    }
+}
+
